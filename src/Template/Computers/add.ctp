ba5b40e99fa7564da44104d18875b1d59732c743
joaobr4z <joao.braz@aluno.imepac.edu.br>
2022-10-22 14:49:33 -0300

    att3

--- src/Template/Computers/add.ctp
+++ src/Template/Computers/add.ctp
@@ -10,10 +10,10 @@
         <section>
             <h1>Cadastro do PC</h1>
             <form action="#">
-                <input type="text" id="tamanho_monitor" name="tamanho_monitor" placeholder="Qual é o tamanho do monitor?">
+                <input type="text" id="tamanho_monitor" name="tamanho_monitor" placeholder="Qual o tamanho do monitor?">
                 <input type="text" id="quantidade_mem_ram" name="quantidade_mem_ram" placeholder="Quantos Gb de Mem Ram?">
                 <input type="text" id="ssd_gigas" name="ssd_gigas" placeholder="Qual a capacidade do SSD? ">
-                <input type="text" id="teclado " name="teclado" placeholder="Qual a marca do teclado?">
+                <input type="text" id="teclado" name="teclado" placeholder="Qual a marca do teclado?">
                 <button type="submit" onclick="validate()">ENVIAR</button>
             </form>
         </section>
--- webroot/js/js_computer.js
+++ webroot/js/js_computer.js
@@ -1,6 +1,18 @@
 function validate(){
-    if(!(document.getElementById("tamanho_monitor").value && document.getElementById("quantidade_mem_ram").value  && document.getElementById("ssd_gigas").value)){
-        alert("Você deve preencher todos os campos! ")
+    if(!(document.getElementById("tamanho_monitor").value)){
+        alert("Você deve preencher o campo que pergunta sobre o monitor!")
+        return false;
+    }
+    if(!(document.getElementById("ssd_gigas").value)){
+        alert("Você deve preencher o campo que pergunta sobre o ssd!")
+        return false;
+    }
+    if(!(document.getElementById("quantidade_mem_ram").value)){
+        alert("Você deve preencher o campo que pergunta sobre a quantidade de memoria ram!")
+        return false;
+    }
+    if(!(document.getElementById("teclado").value)){
+        alert("Você deve preencher o campo que pergunta sobre o teclado!")
         return false;
     }
 }
